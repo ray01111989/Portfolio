745ab8403fdd98d3bfd6f05bb5de2afa57909886
ray01111989 <55462898+ray01111989@users.noreply.github.com>
2022-07-10 20:05:22 -0700

    updated JS file

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link');
+const form = document.getElementById('web-form');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -10,3 +11,12 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+form.addEventListener('submit', function handleSubmit(event) {
+  event.preventDefault();
+
+  // 👇️ Send data to server here
+  form.submit();
+  // 👇️ Reset form here
+  form.reset();
+});
